04495d7341a040ddd8d248e3c876f368e225b888
Varitaxxx <post@asgardschmiede.eu>
2023-02-21 08:11:28 +0100

    Initial commit

---
+++ src/main/java/eu/asgardschmiede/petregisterclient/PetregisterClientApplication.java
@@ -0,0 +1,13 @@
+package eu.asgardschmiede.petregisterclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetregisterClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetregisterClientApplication.class, args);
+	}
+
+}
